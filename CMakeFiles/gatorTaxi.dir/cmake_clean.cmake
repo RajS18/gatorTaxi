1b2876281fd31ef712b5c3b8c94f10b1b1699549
Raj Shukla <rajdragatron918@gmail.com>
2023-04-11 23:07:42 -0400

    Completion

---
+++ CMakeFiles/gatorTaxi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gatorTaxi.dir/gatorTaxi.cpp.obj"
+  "CMakeFiles/gatorTaxi.dir/gatorTaxi.cpp.obj.d"
+  "gatorTaxi.exe"
+  "gatorTaxi.exe.manifest"
+  "gatorTaxi.pdb"
+  "libgatorTaxi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gatorTaxi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
